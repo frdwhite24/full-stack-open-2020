5ad9982dd0fabcd73f35fc557c3a8420ac1d3832
Fred White <fred-white@hotmail.co.uk>
2020-10-04 20:31:37 +0100

    part 4 exercises 4.1 to 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const app = express();
+
+logger.info(`connecting to ${config.MONGODB_URI}`);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error(`error connecting to MongoDB: ${error.message}`);
+  });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
