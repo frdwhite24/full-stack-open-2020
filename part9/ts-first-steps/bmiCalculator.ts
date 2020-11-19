7e82338b84c3da446d1c98547b73304942af4e6f
Fred White <fred-white@hotmail.co.uk>
2020-11-19 22:31:20 +0000

    part 9 exercise 9.1

---
+++ part9/ts-first-steps/bmiCalculator.ts
@@ -0,0 +1,22 @@
+const calculateBmi = (height: number, weight: number): string => {
+  // To account for input of cm
+  if (height > 5) {
+    height = height / 100;
+  }
+
+  const bmi: number = weight / (height * height);
+
+  if (bmi < 18.5) {
+    return "Low (underweight)";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "High (overweight)";
+  } else if (bmi >= 30) {
+    return "Very High (obese)";
+  } else {
+    throw new Error("Unable to calculate BMI");
+  }
+};
+
+console.log(calculateBmi(180, 74));
